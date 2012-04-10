2fa067c9422647b3b301cb4b9a63f65b3f796900
Kathleen Schadeck <kschadeck@gmail.com>
2012-04-09 19:58:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birds::Application.config.secret_token = 'cd2c75b07b876a2832d9e22e1c78664a96b0652adf9c3fd32b36a82809361aef950eba10af979157b44513af9228bcc93bba44a053e952051d77d6ae5237dc0c'
